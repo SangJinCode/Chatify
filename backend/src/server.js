5fe58e9bc4bf479f5284e03312399b4297b48388
SangJinCode <youremail@example.com>
2025-12-05 11:30:24 -0800

    changed package.json for sevalla deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,9 +14,9 @@ const app = express();
 
 const PORT = ENV.PORT || 3000;
 
-app.get("/", (req,res) => {
-    res.send("Hello World!");
-})
+// app.get("/", (req,res) => {
+//     res.send("Hello World!");
+// })
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
